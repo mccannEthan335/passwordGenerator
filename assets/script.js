4346b7f1529e16d17f1cad5bbe7d930721f1b9bf
Edawgg <emccannbusiness@gmail.com>
2022-08-28 12:35:07 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@ var numbers
 var characters
 var capLetters
 var specials
+var choices
 
-//password variables
+//password variables; numbers, lowercase & uppercase, and special characters
 
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
@@ -13,4 +14,12 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
  'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=',
- '{', '[', '}', ']', '|', '/', '\:', '\;', '?', ',', '.']
+ '{', '[', '}', ']', '|', '/', '\:', '\;', '?', ',', '.'];
+
+//function to get uppercase, and declaring a variable for converting
+
+var toUpper = function (letter) {
+  return letter.toUpperCase();
+}
+
+var capatilize = characters.map(toUpper);
