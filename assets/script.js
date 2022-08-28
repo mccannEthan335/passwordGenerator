fcf117e4627130d0743aa27fd0a8ff5c8177295e
Edawgg <emccannbusiness@gmail.com>
2022-08-28 12:22:25 -0500

    declaring variables for password

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//setting variables for user input/ answers to criteria
+var submit
+var numbers
+var characters
+var capLetters
+var specials
+
+//password variables
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+ 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=',
+ '{', '[', '}', ']', '|', '/', '\:', '\;', '?', ',', '.']
